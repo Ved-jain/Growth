be8391b299f50f85a89f9a61efa4a6326e54529c
Ved Jain <vedjain5314@gmail.com>
2026-08-06 01:41:26 +0530

    Time: 64 ms (36.19%) | Memory: 128 MB (15.65%) - LeetSync

---
+++ everything/2686-minimum-cost-of-a-path-with-special-roads/minimum-cost-of-a-path-with-special-roads.cpp
@@ -0,0 +1,94 @@
+class Solution {
+public:
+
+    long long minimumCost(vector<int>& start, vector<int>& target, vector<vector<int>>& specialRoads) {
+
+        vector<pair<int,int>> points;
+
+        // add start and target
+        points.push_back({start[0],start[1]});
+        points.push_back({target[0],target[1]});
+
+        // add all endpoints of special roads
+        for(auto &r : specialRoads){
+            points.push_back({r[0],r[1]});
+            points.push_back({r[2],r[3]});
+        }
+
+        // assign every unique point an id
+        map<pair<int,int>,int> mp;
+        vector<pair<int,int>> nodes;
+
+        for(auto p : points){
+            if(mp.count(p)==0){
+                mp[p]=nodes.size();
+                nodes.push_back(p);
+            }
+        }
+
+        int n = nodes.size();
+
+        vector<vector<pair<int,int>>> adj(n);
+
+        // add normal walking edges
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+
+                if(i==j) continue;
+
+                int x1 = nodes[i].first;
+                int y1 = nodes[i].second;
+
+                int x2 = nodes[j].first;
+                int y2 = nodes[j].second;
+
+                int cost = abs(x1-x2)+abs(y1-y2);
+
+                adj[i].push_back({j,cost});
+            }
+        }
+
+        // add special roads
+        for(auto &r : specialRoads){
+
+            int u = mp[{r[0],r[1]}];
+            int v = mp[{r[2],r[3]}];
+
+            adj[u].push_back({v,r[4]});
+        }
+
+        // Dijkstra
+        vector<long long> dist(n,LLONG_MAX);
+
+        priority_queue<
+            pair<long long,int>,
+            vector<pair<long long,int>>,
+            greater<pair<long long,int>>
+        > pq;
+
+        int src = mp[{start[0],start[1]}];
+        int dest = mp[{target[0],target[1]}];
+
+        dist[src]=0;
+        pq.push({0,src});
+
+        while(!pq.empty()){
+
+            auto [d,node]=pq.top();
+            pq.pop();
+
+            if(d>dist[node]) continue;
+
+            for(auto [child,cost]:adj[node]){
+
+                if(dist[child]>d+cost){
+
+                    dist[child]=d+cost;
+                    pq.push({dist[child],child});
+                }
+            }
+        }
+
+        return dist[dest];
+    }
+};
