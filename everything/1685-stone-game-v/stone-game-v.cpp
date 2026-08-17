2702c0f9ee79061df6c75d71f8e80e190cd1127f
Ved Jain <vedjain5314@gmail.com>
2026-08-17 17:24:42 +0530

    Time: 511 ms (43.17%) | Memory: 27.7 MB (33.99%) - LeetSync

--- everything/1685-stone-game-v/stone-game-v.cpp
+++ everything/1685-stone-game-v/stone-game-v.cpp
@@ -3,29 +3,31 @@ public:
     vector<vector<int>> dp;
     vector<int> pref;
 
-    int sum(int l, int r) {
+    int sum(int l, int r){
         return pref[r + 1] - pref[l];
     }
 
-    int solve(int l, int r, vector<int>& st) {
-        if (l == r) return 0;
+    int solve(int l, int r, vector<int>& st){
+        if(l == r) return 0;
 
-        if (dp[l][r] != -1) return dp[l][r];
+        if(dp[l][r] != -1) return dp[l][r];
 
         int ans = 0;
 
-        for (int mid = l; mid < r; mid++) {
-            int leftSum = sum(l, mid);
-            int rightSum = sum(mid + 1, r);
+        for(int mid = l; mid < r; mid++){
+            int left = sum(l, mid);
+            int right = sum(mid + 1, r);
 
-            if (leftSum < rightSum) {
-                ans = max(ans, leftSum + solve(l, mid, st));
+            if(left < right){
+                ans = max(ans, left + solve(l, mid, st));
             }
-            else if (rightSum < leftSum) {
-                ans = max(ans, rightSum + solve(mid + 1, r, st));
+
+            else if(right < left){
+                ans = max(ans, right + solve(mid + 1, r, st));
             }
-            else {
-                ans = max(ans, leftSum + max(solve(l, mid, st), solve(mid + 1, r, st)));
+
+            else{
+                ans = max(ans, left + max(solve(l, mid, st), solve(mid + 1, r, st)));
             }
         }
 
@@ -38,7 +40,7 @@ public:
         dp.assign(n, vector<int>(n, -1));
 
         pref.resize(n + 1, 0);
-        
+
         for (int i = 0; i < n; i++){
             pref[i + 1] = pref[i] + st[i];
         }
